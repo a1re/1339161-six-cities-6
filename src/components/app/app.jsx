d1ebb099c2e89a1d4788c613914297c095b25fd9
Yuriy Marin <yuriy.marin@gmail.com>
2021-02-08 00:03:45 +0300

    feat: Добавлен роутинг в компонент App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import Main from '../main/main';
+import MainScreen from '../main-screen/main-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
+import LoginScreen from '../login-screen/login-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import PropertyScreen from '../property-screen/property-screen';
+
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 
 const App = ({cities, offers, defaultCity, authorizedUser}) => {
-  return <Main cities={cities} offers={offers} city={defaultCity} authorizedUser={authorizedUser} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen cities={cities} offers={offers} city={defaultCity} authorizedUser={authorizedUser} />;
+        </Route>
+        <Route exact path="/login">
+          <LoginScreen />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen offers={offers} authorizedUser={authorizedUser} />;
+        </Route>
+        <Route exact path="/offer/:id">
+          <PropertyScreen authorizedUser={authorizedUser} />;
+        </Route>
+        <Route>
+          <NotFoundScreen authorizedUser={authorizedUser} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
