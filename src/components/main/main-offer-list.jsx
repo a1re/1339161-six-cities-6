61b261acfc9019adcea2cc45a9b9992946592736
Yuriy Marin <yuriy.marin@gmail.com>
2021-03-07 02:21:31 +0300

    feat:  Добавлена сортировка предложений

--- src/components/main/main-offer-list.jsx
+++ src/components/main/main-offer-list.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {ActionCreator} from '../../store/action';
 import CustomPropTypes from '../../custom-prop-types';
 import MainSorting from './main-sorting';
 import MainOfferCard from './main-offer-card';
 import Map from '../map/map';
+import {connect} from 'react-redux';
+import {ActionCreator} from '../../store/action';
+import {Sorting} from '../../const';
 
 const MainOfferList = ({offersInCity, onSelectOffer, selectedCity}) => {
+  const [sortingMethod, setSortingMethod] = useState(Object.entries(Sorting).find(([, method]) => method.isDefault)[1]);
+  const offers = [...offersInCity].sort(sortingMethod.callback);
 
   return (<div className="cities__places-container container">
     <section className="cities__places places">
       <h2 className="visually-hidden">Places</h2>
-      <b className="places__found">{offersInCity.length} {offersInCity.length > 1 ? `places` : `place`} to stay in {selectedCity.name}</b>
-      <MainSorting />
+      <b className="places__found">{offers.length} {offers.length > 1 ? `places` : `place`} to stay in {selectedCity.name}</b>
+      <MainSorting activeMethodName={sortingMethod.name} onSelectMethod={setSortingMethod}/>
       <div className="cities__places-list places__list tabs__content">
-        {offersInCity.map((offer) => <MainOfferCard
+        {offers.map((offer) => <MainOfferCard
           key={`offer-card-${offer.id}`}
           offer={offer}
           onHoverIn={() => onSelectOffer(offer.id) }
--- src/components/main/main-sorting.jsx
+++ src/components/main/main-sorting.jsx
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import {Sorting, ESC_KEYCODE} from '../../const';
 
-const MainSorting = () => (
-  <form className="places__sorting" action="#" method="get">
+const MainSorting = ({activeMethodName, onSelectMethod}) => {
+  const [isSortingOpen, openSorting] = useState(false);
+
+  const closeSortingByEsc = (evt) => {
+    if (evt.keyCode === ESC_KEYCODE) {
+      openSorting(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener(`keydown`, closeSortingByEsc, false);
+
+    return () => {
+      document.removeEventListener(`keydown`, closeSortingByEsc, false);
+    };
+  }, []);
+
+  return <form className="places__sorting" action="#" method="get">
     <span className="places__sorting-caption">Sort by</span>
     &nbsp;
-    <span className="places__sorting-type" tabIndex={0}>
-      Popular
+    <span className="places__sorting-type" tabIndex={0} onClick={() => openSorting(!isSortingOpen)}>
+      {activeMethodName}
       <svg className="places__sorting-arrow" width={7} height={4}>
         <use xlinkHref="#icon-arrow-select" />
       </svg>
     </span>
-    <ul className="places__options places__options--custom places__options--opened">
-      <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-      <li className="places__option" tabIndex={0}>Price: low to high</li>
-      <li className="places__option" tabIndex={0}>Price: high to low</li>
-      <li className="places__option" tabIndex={0}>Top rated first</li>
+    <ul className={classNames(`places__options`, `places__options--custom`, isSortingOpen && `places__options--opened`)}>
+      {Object.entries(Sorting).map(([methodKey, method]) =>
+        <li
+          key={methodKey}
+          className={classNames(`places__option`, activeMethodName === method.name && `places__option--active`)}
+          tabIndex={0}
+          onClick={() => {
+            onSelectMethod(method);
+            openSorting(false);
+          }}
+        >
+          {method.name}
+        </li>)
+      }
     </ul>
-  </form>
-);
+  </form>;
+};
+
+MainSorting.propTypes = {
+  activeMethodName: PropTypes.string.isRequired,
+  onSelectMethod: PropTypes.func.isRequired
+};
 
 export default MainSorting;
