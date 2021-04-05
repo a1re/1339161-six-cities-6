e8bdb586c810bfae05a29b0593f6c42defc23a13
Yuriy Marin <yuriy.marin@gmail.com>
2021-04-06 01:51:29 +0300

    fix: Убран индекс массива из key в map

--- src/components/room/room-info.jsx
+++ src/components/room/room-info.jsx
@@ -7,7 +7,7 @@ const RoomInfo = ({offer}) => {
   return <React.Fragment>
     <div className="property__gallery-container container">
       <div className="property__gallery">
-        {offer.images.map((image, i) => (<div className="property__image-wrapper" key={`image-${i}`}>
+        {offer.images.map((image) => (<div className="property__image-wrapper" key={image}>
           <img className="property__image" src={image} alt="Photo studio" />
         </div>))}
       </div>
